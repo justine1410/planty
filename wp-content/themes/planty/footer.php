486b1032f44e81a4489bbb4ff7005922d58148b2
justine <justine1410@live.fr>
2023-04-04 19:22:47 +0200

    Création du header et du footer

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+
+<footer id="site-footer" class="header-footer-group">
+				<a href="#">Mentions Légales</a>
+</footer>
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ wp-content/themes/planty/header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body >
+	<header class="header">
+		<div class="logo">
+			<a href="#">
+				<img src="http://localhost/planty/wp-content/uploads/2023/04/Logo-2.png" alt="Logo_planty">
+			</a>
+			<p>energy drink</p>
+
+		</div>
+		<div class="navigation_commande">
+			<nav>
+				<ul>
+					<li> <a href="#">Nous rencontrer</a>  </li>
+					<li><a href="#">Admin</a></li>
+				</ul>
+			</nav>
+			<button>
+				Commander
+</button>
+		</div>
+	</header>
+
+</body>			
+
+
